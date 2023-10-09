ac443b08f9a1c5a883244f379905b8a8c0cba31b
Karmesin924 <jinhong0924@naver.com>
2023-10-09 18:37:37 +0900

    Vercel 404 error 설정

--- src/components/main/MainTitle.jsx
+++ src/components/main/MainTitle.jsx
@@ -5,7 +5,10 @@ const MainTitle = () => {
     <div className={styles.main_title}>
       <p className={styles.title_light}>MBTI로 알아보는</p>
       <p className={styles.title_bold}>나와 어울리는 고양이 찾기</p>
-      <img className={styles.title_img} src="public/cats.png" />
+      <img
+        className={styles.title_img}
+        src="https://github.com/Karmesin924/mbti-test/blob/main/public/cats.png?raw=true"
+      />
     </div>
   );
 };
