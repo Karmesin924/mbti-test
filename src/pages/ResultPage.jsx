f5a4a10d130888532d8930dfc57393d942b789cd
Karmesin924 <jinhong0924@naver.com>
2023-10-09 18:26:28 +0900

    MBTI 테스트 완성

---
+++ src/pages/ResultPage.jsx
@@ -0,0 +1,26 @@
+import { useParams } from 'react-router-dom';
+import { resultData } from '../constants/resultData';
+import styles from '../App.module.css';
+
+const ResultPage = () => {
+  const { mbti } = useParams();
+
+  const resultMBTIData = resultData.find((data) => data.mbti === mbti);
+  const resultDesc = resultMBTIData.desc.split('\n');
+  return (
+    <div className={styles.layout}>
+      <p className={styles.title_light}>나와 어울리는 고양이는?</p>
+      <p className={styles.title_bold}>
+        {resultMBTIData.match_cat} ({mbti})
+      </p>
+      <img className={styles.result_img} src={resultMBTIData.image} />
+      {resultDesc.map((desc, index) => (
+        <p key={index} className={styles.title_light}>
+          {desc}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default ResultPage;
